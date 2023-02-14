127a0ab5c16d98e74827493b57fbd9cb1d86f1f1
LeoVeriMagnum <leonardoverissimo1223@proton.me>
2023-02-14 12:13:32 -0300

    Testes

---
+++ aula1/exemplo1.php
@@ -0,0 +1,9 @@
+<?php 
+    $ola = "Olá Mundo";
+    echo gettype($ola);
+    echo "<br>";
+    $ola = 10;
+    echo $ola;
+    echo gettype($ola);
+    echo "<br>";
+?>
---
+++ aula1/exercicio3.php
@@ -0,0 +1,14 @@
+<?php
+$a = "leoaaaaa";
+$b=0;
+
+while ($b!==10000){
+    $a++;
+    
+    //$b++;
+    if($a === "leonardo"){
+        echo $a;
+        break;
+    } 
+}
+?>
