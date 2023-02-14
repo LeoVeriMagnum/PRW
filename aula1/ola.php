9dbc696ee199f33bb966d2051d0035fa374c848d
LeoVeriMagnum <leonardoverissimo1223@proton.me>
2023-02-14 11:00:53 -0300

    meu primeiro programa php

---
+++ aula1/ola.php
@@ -0,0 +1,13 @@
+<?php
+    echo "
+    <ol>
+        <li><h1>Ola Mundo<h1></li>
+        <li><h1>Ola Mundo<h1></li>
+        <li><h1>Ola Mundo<h1></li>
+        <li><h1>Ola Mundo<h1></li>
+        <li><h1>Ola Mundo<h1></li>
+        <li><h1>Ola Mundo<h1></li>
+        
+    </ol>";
+    
+?>
