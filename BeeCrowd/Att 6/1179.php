baf87078aee578a057fe10f2334488df7ce2f943
LeoVeriMagnum <leonardoverissimo1223@proton.me>
2023-04-03 09:04:37 -0300

    Não terminado

---
+++ BeeCrowd/Att 6/1179.php
@@ -0,0 +1,21 @@
+<?php
+
+$x = array();
+$par = array();
+$impar = array();
+
+for($i=0; $i<15; $i++){
+    if(count($par)==5){
+        for($n=0;$n<)
+    }
+    $x[$i] = readline();
+    if(($x[$i] % 2) == 0){
+        array_push($par, $x[$i]);
+    } else {
+        array_push($impar, $x[$i]);
+    }
+}
+
+
+
+?>
